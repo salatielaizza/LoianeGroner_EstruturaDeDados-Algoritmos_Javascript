8c48b6e74c261354aabbaaf867cb0c728a0c4066
Salatiel Aizza <salatiel.aizza@gmail.com>
2021-01-18 01:45:40 +0000

    Estudo de Array Bidimensional

---
+++ AverageTemperatures.js
@@ -0,0 +1,24 @@
+let averageTemp = []
+averageTemp[0] = [15, 18, 09, 13, 15, 16, 17];
+averageTemp[1] = [10, 11, 12, 13, 09, 16, 14];
+
+function printMatriz(averageTemp){
+    for (let i = 0; i < averageTemp.length; i++) {
+        for (let j = 0; j < averageTemp[i].length; j++) {
+        }
+    }
+    console.table(averageTemp);
+}
+printMatriz(averageTemp);
+
+let matriz2 = [];
+
+matriz2[0] = [];
+matriz2[0][0] = 10;
+matriz2[0][1] = 10;
+matriz2[1] = [];
+matriz2[1][0] = 11;
+matriz2[1][1] = 11;
+console.log(matriz2);
+
+//teste: acrescentei comentário apenas para testar adicionar na origin
